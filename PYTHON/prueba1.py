52045b614dae7a73d2dce9ff047c759ca0a89698
joejhona <joejhona@gmail.com>
2019-09-26 10:24:43 -0500

    Con Cartopy

--- PYTHON/prueba1.py
+++ PYTHON/prueba1.py
@@ -1,15 +1,7 @@
-#from netCDF4 import Dataset
-#import matplotlib.pyplot as plt
-#from matplotlib.cm import get_cmap
-#import cartopy.crs as crs
-#from cartopy.feature import NaturalEarthFeature
-
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
-from mpl_toolkits.basemap import Basemap
-
-from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim,
-                 cartopy_ylim, latlon_coords)
+import cartopy.crs as crs
+from cartopy.feature import NaturalEarthFeature
 
 print('todo bien /data/users/jticse/WRFV4/wrfout/periodos ===> wrfout_d02_2018-02-17_00:00:00')
