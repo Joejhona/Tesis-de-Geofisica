e3090b05c149bfb382658ec8f8f6514764d06d08
joejhona <joejhona@gmail.com>
2019-09-26 10:12:33 -0500

    probando librerias

--- PYTHON/prueba1.py
+++ PYTHON/prueba1.py
@@ -1,1 +1,10 @@
-print("joel")
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+import cartopy.crs as crs
+from cartopy.feature import NaturalEarthFeature
+
+from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim,
+                 cartopy_ylim, latlon_coords)
+
+print('todo bien /data/users/jticse/WRFV4/wrfout/periodos ===> wrfout_d02_2018-02-17_00:00:00')
