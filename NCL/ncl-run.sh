1cc7c2d7a2142fe2e856df7c8ee9857bcc0571e1
Joejhona-Cluster <joejhona@gmail.com>
2019-10-23 19:07:13 -0500

    andadno

--- NCL/ncl-run.sh
+++ NCL/ncl-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 module load intel/16.0.3
 module load intel_ncl/6.4.0
-ncl /data/users/jticse/Tesis-Maestria/SALIDAS/scripts/Tesis-de-Geofisica/NCL/anom-wrf-pisco.ncl
+ncl anom-wrf-pisco.ncl
